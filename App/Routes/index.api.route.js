e3e7ee855808f2f08bd1dd11a25732fffb3c9019
p77u4n <ptuankhang@gmail.com>
2016-11-19 01:34:34 +0700

    test researchers db

--- App/Routes/index.api.route.js
+++ App/Routes/index.api.route.js
@@ -1,4 +1,4 @@
-
+var jwt = require('jsonwebtoken');
 
 module.exports =  function(app) {
     var express = require('express');
@@ -15,6 +15,29 @@ module.exports =  function(app) {
             res.json(researchers);
         }) 
     });
+
+    apiRouter.post('/authentication', function(req, res) {
+        ResearcherInfoModel.findOne({usrname: req.usrname}, function(err, researcher) {
+            if(err) {console.log(err);}
+            else {
+                console.log(req.body.password);
+                if(!researcher) {
+                    res.json({success : false, message: 'You must register before being our Researcher'});
+                }else{
+                    if(req.body.password != researcher.password) {
+                        res.json({success : false, message : 'Password is not matched'});
+                    }else{
+                        var token = jwt.sign(researcher, app.get('superSecret'), {
+                            expiresIn : 1440 // expires in 24 hours
+                        });
+
+                        res.json({success : true, message : 'Enjoy your research activity', token});
+                    }
+                }
+            }
+            
+        })
+    })
 
     app.use('/api', apiRouter);
 }
